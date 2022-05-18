facae2c2ea18e20d37810b91d9c2f876e5c979b0
Igor GIT <IgorGIT@users.noreply.github.com>
2022-05-18 09:39:41 +0300

    Revert "renamed page to second"
    This reverts commit 554502309e59048f39461369d897597c75f4c096.

--- infra_project/infra_app/tests.py
+++ infra_project/infra_app/tests.py
@@ -7,20 +7,18 @@ class StaticPagesURLTests(TestCase):
     def setUp(self):
         self.guest_client = Client()
 
-    def test_about_url1_exists_at_desired_location(self):
+    def test_about_url_exists_at_desired_location(self):
         """Проверка доступности страниц."""
         response = self.guest_client.get('/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
-    def test_about_url2_exists_at_desired_location(self):
-        response = self.guest_client.get('/second/')
+        response = self.guest_client.get('/second_page/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
-    def test_page1_shows_correct_content(self):
+    def test_page_shows_correct_content(self):
         """Проверка контента страниц."""
         response = self.guest_client.get('/')
         self.assertContains(response, 'У меня получилось!')
 
-    def test_page2_shows_correct_content(self):
-        response = self.guest_client.get('/second/')
+        response = self.guest_client.get('/second_page/')
         self.assertContains(response, 'А это вторая страница!')
